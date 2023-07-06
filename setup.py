1e79054e684cbcc4ff0509f087b6f71c3f6b9b81
patquem <patrick.quemere@cea.fr>
2023-07-06 18:02:08 +0200

    add setup.cfg and setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fitspy",
+    version='1.0.0',
+    license='GPLv3',
+    setup_requires=['setuptools_scm'],
+    include_package_data=True,
+    zip_safe=False,
+    python_requires='>=3.7',
+    install_requires=[
+        "numpy",
+        "pandas",
+        "matplotlib",
+        "scipy",
+        "lmfit",
+        "json",
+        "win32clipboard; platform_system == 'Windows'",
+    ],
+    packages=find_packages(),
+
+    description="Fitspy (A generic tool to fit spectra in python)",
+
+    url="https://github.com/CEA-MetroCarac/fitspy",
+    author_email="patrick.quemere@cea.fr",
+    author="Patrick Quéméré",
+    keywords="Fitspy, fit, spectra, spectrum, map, 1D, 2D, decomposition, Gaussian, Lorentzian, Pseudovoigt, GUI",
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: GPL-3.0 license',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Environment :: Console',
+    ],
+)
