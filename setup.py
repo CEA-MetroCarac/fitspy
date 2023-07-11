6db1d53b1c575bdb305f7377444ae10f4d80e482
Matthew Bryan <78845903+matbryan52@users.noreply.github.com>
2023-07-11 07:59:16 +0200

    Add extra classifiers in setup.py

--- setup.py
+++ setup.py
@@ -27,6 +27,10 @@ setup(
              "Gaussian, Lorentzian, Pseudovoigt, GUI",
     classifiers=[
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',        
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX :: Linux',
