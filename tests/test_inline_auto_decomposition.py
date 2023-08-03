edc4e066b9f42280c6202b74d9a119c8d0a0c6b0
patquem <patrick.quemere@cea.fr>
2023-08-03 15:59:35 +0200

    add relative tol.

--- tests/test_inline_auto_decomposition.py
+++ tests/test_inline_auto_decomposition.py
@@ -21,5 +21,5 @@ def test_inline_auto_decomposition():
     assert len(spectra) > 0
     for spectrum, ref in zip(spectra, refs):
         ampli = spectrum.models[0].param_hints['ampli']['value']
-        assert np.sum(spectrum.y) == approx(ref[0])
+        assert np.sum(spectrum.y) == approx(ref[0], rel=1e-2)
         assert ampli == approx(ref[1], rel=1e-2)
