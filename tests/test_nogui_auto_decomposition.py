9b30dceb490e0d488c80976145e7cb961d4a67f0
patquem <patrick.quemere@cea.fr>
2024-08-05 11:12:58 +0200

    make test_gui_auto_decomposition and test_nogui_auto_decomposition similar

--- tests/test_nogui_auto_decomposition.py
+++ tests/test_nogui_auto_decomposition.py
@@ -2,24 +2,21 @@ import numpy as np
 import pytest
 from pytest import approx
 
+from tests.test_gui_auto_decomposition import REFS
+
 from examples.ex_nogui_auto_decomposition import auto_decomposition
 
 
 def test_auto_decomposition():
     spectra = auto_decomposition(verbosity=False)
 
-    # refs = []
-    # for i in range(3):
-    #     ampli = spectra[i].peak_models[0].param_hints['ampli']['value']
-    #     refs.append([np.sum(spectra[i].y), ampli])
-    # print(refs)
+    results = []
+    for i in range(3):
+        x0 = spectra[i].peak_models[0].param_hints['x0']['value']
+        ampli = spectra[i].peak_models[0].param_hints['ampli']['value']
+        fwhm = spectra[i].peak_models[0].param_hints['fwhm']['value']
+        results.append([x0, ampli, fwhm])
+    # print(results)
 
-    refs = [[695742.9828417138, 28035.104052186613],
-            [645910.1509814768, 18783.270429923425],
-            [588544.9856527029, 8125.02943468474]]
-
-    assert len(spectra) > 0
-    for spectrum, ref in zip(spectra, refs):
-        ampli = spectrum.peak_models[0].param_hints['ampli']['value']
-        assert np.sum(spectrum.y) == approx(ref[0], rel=1e-2)
-        assert ampli == approx(ref[1], rel=1e-2)
+    for result, reference in zip(results, REFS):
+        assert result == approx(reference, rel=2e-2)
