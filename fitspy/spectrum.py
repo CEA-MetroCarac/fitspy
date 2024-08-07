bc62557a305b23253ca1e077f2889bb90cd38044
patquem <patrick.quemere@cea.fr>
2024-08-07 09:38:16 +0200

    (re)introduce conditions for plotting baseline

--- fitspy/spectrum.py
+++ fitspy/spectrum.py
@@ -694,7 +694,9 @@ class Spectrum:
             ax.hlines(y=y_noise_level, xmin=x[0], xmax=x[-1], colors='r',
                       linestyles='dashed', lw=0.5, label="Noise level")
 
-        if show_baseline and self.baseline.y_eval is not None:
+        if show_baseline and self.baseline.is_subtracted:
+            if self.baseline.y_eval is None:
+                self.baseline.eval(x, self.y_no_outliers)
             ax.plot(x, self.baseline.y_eval, 'g', label="Baseline")
 
         y_bkg = np.zeros_like(x)
