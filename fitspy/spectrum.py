559cd94f343b8b54a8f04c3e6f62f510f0bf9d9b
patquem <patrick.quemere@cea.fr>
2024-08-28 12:29:32 +0200

    bugfix: replace spectrum. by self.

--- fitspy/spectrum.py
+++ fitspy/spectrum.py
@@ -131,10 +131,10 @@ class Spectrum:
 
     def reinit(self):
         """ Reinitialize the main attributes """
-        self.range_min = spectrum.x0.min()
-        self.range_max = spectrum.x0.max()
-        self.x = spectrum.x0.copy()
-        self.y = spectrum.y0.copy()
+        self.range_min = self.x0.min()
+        self.range_max = self.x0.max()
+        self.x = self.x0.copy()
+        self.y = self.y0.copy()
         self.normalize = False
         self.normalize_range_min = None
         self.normalize_range_max = None
