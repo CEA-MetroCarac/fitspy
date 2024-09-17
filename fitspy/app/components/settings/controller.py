645bc1748f924e690cd1a6b56f54b9be372efa7e
patquem <patrick.quemere@cea.fr>
2024-09-17 15:35:09 +0200

    update save_model() from current_fit_model

--- fitspy/app/components/settings/controller.py
+++ fitspy/app/components/settings/controller.py
@@ -2,6 +2,7 @@ import json
 from PySide6.QtCore import QObject, Signal
 from PySide6.QtWidgets import QFileDialog
 from .model import Model
+from fitspy.core.utils import save_to_json
 
 
 class SettingsController(QObject):
@@ -22,16 +23,12 @@ class SettingsController(QObject):
     def setup_connections(self):
         self.model.currentModelChanged.connect(self.update_model)
         self.solver_settings.outliers_coef.valueChanged.connect(
-            lambda value: self.settingChanged.emit("outliers_coef", value)
-        )
+            lambda value: self.settingChanged.emit("outliers_coef", value))
         self.solver_settings.outliers_removal.clicked.connect(self.removeOutliers)
-
         self.export_settings.save_only_path.stateChanged.connect(
-            lambda state: self.settingChanged.emit("save_only_path", state == 2)
-        )
-
-        model_settings = self.model_builder.model_settings
-        model_settings.save_button.clicked.connect(self.save_model_settings)
+            lambda state: self.settingChanged.emit("save_only_path", state == 2))
+        self.model_builder.model_settings.save_button.clicked.connect(
+            self.save_model_settings)
 
     def set_model(self, spectrum):
         model = spectrum.save()
@@ -42,44 +39,10 @@ class SettingsController(QObject):
         self.fit_settings.update_model(fit_model)
 
     def save_model_settings(self):
-        spectral_range = self.model_builder.model_settings.spectral_range
-        baseline = self.model_builder.model_settings.baseline
-        normalization = self.model_builder.model_settings.normalization
-        fitting = self.model_builder.model_settings.fitting
-
-        params = {}
-
-        params['spectral_range'] = {
-            'x_min': spectral_range.x_min.value(),
-            'x_max': spectral_range.x_max.value()}
-
-        params['baseline'] = {
-            'semi_auto': baseline.radio_semi_auto.isChecked(),
-            'linear': baseline.radio_linear.isChecked(),
-            'polynomial': baseline.radio_polynomial.isChecked(),
-            'polynomial_order': baseline.spin_polynomial_order.value(),
-            'attached': baseline.attached.isChecked(),
-            'sigma': baseline.spin_sigma.value()}
-
-        params['normalization'] = {
-            'x_min': normalization.x_min.value(),
-            'x_max': normalization.x_max.value(),
-            'normalize': normalization.normalize.isChecked()}
-
-        params['fit_models'] = {
-            'background_model': fitting.background_model.currentText(),
-            'peak_model': fitting.peak_model.currentText()}
-
-        params['fit_settings'] = {
-            'fit_negative': self.fit_settings.fit_negative_checkbox.isChecked(),
-            'fit_outliers': self.fit_settings.fit_outliers_checkbox.isChecked(),
-            'coef_noise': self.fit_settings.coef_noise_input.value(),
-            'max_iterations': self.fit_settings.max_iterations_input.value(),
-            'method': self.fit_settings.fit_method_combo.currentText(),
-            'xtol': self.fit_settings.xtol_input.value()}
+        fit_model = self.model.current_fit_model
+        fit_model.pop("fname")
 
         types = "JSON Files (*.json);;All Files (*)"
         fname = QFileDialog.getSaveFileName(None, "Save File", "", types)[0]
         if fname:
-            with open(fname, 'w') as file:
-                json.dump(params, file, indent=4)
+            save_to_json(fname, fit_model)
