00c518504a483b5056338803f0196cc7ecc334e5
patquem <patrick.quemere@cea.fr>
2025-01-23 09:48:09 +0100

    use generic fitspy_launcher() from fitspy.apps

--- examples/ex_gui_reload_model.py
+++ examples/ex_gui_reload_model.py
@@ -3,30 +3,21 @@ Example of preprocessed spectra model reloading through the application
 """
 from pathlib import Path
 
-from fitspy.apps.pyside.main import fitspy_launcher
-from fitspy.apps.tkinter.gui import fitspy_launcher as fitspy_launcher_tk
+from fitspy.apps import fitspy_launcher
 
 DATA = Path(__file__).parent / "data"
 
 
 def gui_reload_model(gui='pyside'):
     """ Example of preprocessed spectra model reloading through the appli """
-    assert gui in ['pyside', 'tkinter']
 
-    if gui == 'pyside':
-        fitspy_launcher(fname_json=DATA / 'spectra_2' / 'model.json')
-    else:
-        fitspy_launcher_tk(fname_json=DATA / 'spectra_2' / 'model.json')
+    fitspy_launcher(fname_json=DATA / 'spectra_2' / 'model.json', gui=gui)
 
 
 def gui_reload_model_2d_map(gui='pyside'):
     """ Example of preprocessed '2D map' spectra model reloading through the appli """
-    assert gui in ['pyside', 'tkinter']
 
-    if gui == 'pyside':
-        fitspy_launcher(fname_json=DATA / '2D_maps' / 'model.json')
-    else:
-        fitspy_launcher_tk(fname_json=DATA / '2D_maps' / 'model.json')
+    fitspy_launcher(fname_json=DATA / '2D_maps' / 'model.json', gui=gui)
 
 
 if __name__ == "__main__":
