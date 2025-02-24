1a1377792443fb8ac6e067d705f16a1aecf97fc7
Crackvignoule <kiki.kalagan@gmail.com>
2025-02-24 09:33:26 +0100

    v2025.2

--- fitspy/__init__.py
+++ fitspy/__init__.py
@@ -5,7 +5,7 @@ from lmfit.models import ConstantModel, LinearModel, ParabolicModel, Exponential
 from fitspy.core.models import gaussian, lorentzian, gaussian_asym, lorentzian_asym, pseudovoigt
 from fitspy.core.utils import load_models_from_txt, load_models_from_py
 
-VERSION = "2025.1"
+VERSION = "2025.2"
 
 PEAK_MODELS = {"Gaussian": gaussian,
                "Lorentzian": lorentzian,
