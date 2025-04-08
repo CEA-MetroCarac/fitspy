b0482420d52e7e0f106f23d53d6f02164aa93799
patquem <patrick.quemere@cea.fr>
2025-04-08 15:47:01 +0200

    manage tmpdir

--- examples/ex_nogui_weights_in_fit.py
+++ examples/ex_nogui_weights_in_fit.py
@@ -1,57 +1,60 @@
 """
 Example of weights passed to the fit
 """
-import tempfile
 from pathlib import Path
 import matplotlib.pyplot as plt
 
 from fitspy.core.spectrum import Spectrum
 
 
-def ex_nogui_weights_in_fit(show_plots=False):
+def fit(fname, weighted=False):
+    spectrum = Spectrum()
+    spectrum.load_profile(fname)
+    if not weighted:
+        spectrum.weights = None
+    spectrum.set_bkg_model('Linear')
+    spectrum.fit(coef_noise=0)
+    return spectrum
+
+
+def plot(ax, spectrum, label):
+    ax.plot(spectrum.x, spectrum.y, 'k--', lw=0.5, marker='o', mfc='r')
+    ax.plot(spectrum.x, spectrum.result_fit.best_fit, label=label)
+    ax.set_xlim(0, 6)
+    ax.legend()
+
+
+def ex_nogui_weights_in_fit(dirname, show_plots=False):
     """ Example of weights taken into account during the fit processing """
 
-    csv_string = """ # x,y,weights
+    csv_string = """x,y,weights
         1,10,1
         2,20,1
         3,0,0
         4,40,1
         5,50,1
     """
-
-    tmpdir = tempfile.gettempdir()
-    fname = Path(tmpdir) / 'input.csv'
+    fname = Path(dirname) / 'input.csv'
 
     with open(fname, 'w') as fid:
         fid.write(csv_string)
 
-    _, ax = plt.subplots(1, 2, figsize=(8, 4))
-
-    def fit(ax, weighted=False, label=None):
-        spectrum = Spectrum()
-        spectrum.load_profile(fname)
-        if not weighted:
-            spectrum.weights = None
-        spectrum.set_bkg_model('Linear')
-        spectrum.fit(coef_noise=0)
-        best_fit = spectrum.result_fit.best_fit
-
-        ax.plot(spectrum.x, spectrum.y, 'k--', lw=0.5, marker='o', mfc='r')
-        ax.plot(spectrum.x, spectrum.result_fit.best_fit, label=label)
-        ax.set_xlim(0, 6)
-        ax.legend()
-
-        return best_fit
-
-    best_fit_unweighted = fit(ax[0], weighted=False, label='unweighted fit')
-    best_fit_weighted = fit(ax[1], weighted=True, label='weighted fit')
+    spectrum_unweighted = fit(fname, weighted=False)
+    spectrum_weighted = fit(fname, weighted=True)
 
     if show_plots:
+        _, ax = plt.subplots(1, 2, figsize=(8, 4))
+        plot(ax[0], spectrum_unweighted, label='unweighted fit')
+        plot(ax[1], spectrum_weighted, label='weighted fit')
         plt.show()
     else:
-        plt.close()
+        best_fit_unweighted = spectrum_unweighted.result_fit.best_fit
+        best_fit_weighted = spectrum_weighted.result_fit.best_fit
         return best_fit_unweighted, best_fit_weighted
 
 
 if __name__ == "__main__":
-    ex_nogui_weights_in_fit(show_plots=True)
+    import tempfile
+
+    tmpdir = tempfile.gettempdir()
+    ex_nogui_weights_in_fit(tmpdir, show_plots=True)
--- tests/test_nogui_weights_in_fit.py
+++ tests/test_nogui_weights_in_fit.py
@@ -9,8 +9,8 @@ sys.path.insert(0, str(examples_path))
 from ex_nogui_weights_in_fit import ex_nogui_weights_in_fit
 
 
-def test_nogui_reload_model():
-    results = ex_nogui_weights_in_fit()
+def test_nogui_reload_model(tmpdir):
+    results = ex_nogui_weights_in_fit(tmpdir)
 
     assert results[0] == pytest.approx([4., 14., 24., 34., 44.])
     assert results[1] == pytest.approx([10., 20., 30., 40., 50.])
