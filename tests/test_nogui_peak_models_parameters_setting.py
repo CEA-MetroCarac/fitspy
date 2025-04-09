d4f6c1ccb12479108419a255d9c257cb47da1175
patquem <patrick.quemere@cea.fr>
2025-04-09 11:22:32 +0200

    add absolute tolerance in test

--- tests/test_nogui_peak_models_parameters_setting.py
+++ tests/test_nogui_peak_models_parameters_setting.py
@@ -8,10 +8,8 @@ sys.path.insert(0, str(examples_path))
 
 from ex_nogui_peak_models_parameters_setting import ex_nogui_peak_models_parameters_setting
 
-REF = [199.91928386217884, 218.3402514001517, 300, 109.17012570007584]
-
 
 def test_nogui_peak_models_parameters_setting():
     results = ex_nogui_peak_models_parameters_setting()
 
-    assert results == pytest.approx(REF)
+    assert results == pytest.approx([199.9192, 218.3402, 300, 109.17012], abs=1e-3)
