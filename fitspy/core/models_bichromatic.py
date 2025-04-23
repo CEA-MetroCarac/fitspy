30e4f0166cafc8c9fd9c753909f0968d7efb1d9b
patquem <patrick.quemere@cea.fr>
2025-04-23 10:44:47 +0200

    add global var

--- fitspy/core/models_bichromatic.py
+++ fitspy/core/models_bichromatic.py
@@ -52,7 +52,6 @@ def pseudovoigt_ka12(x, ampli, fwhm, x0, alpha=0.5, cathode='Cu'):
     cathode: str
        element of the X-ray source cathode
 
-
     Example
     -------
 
@@ -61,6 +60,7 @@ def pseudovoigt_ka12(x, ampli, fwhm, x0, alpha=0.5, cathode='Cu'):
         import numpy as np
         import matplotlib.pyplot as plt
         from fitspy.core.models_bichromatic import pseudovoigt_ka12
+        from fitspy.core.models_bichromatic import MODE, WAVELENGTH_RATIO, AMPLITUDE_RATIO
 
         x = np.linspace(68, 71, 2000)
         y = pseudovoigt_ka12(x, ampli=100, fwhm=.2, x0=69.14, alpha=0.5, cathode='Cu')
