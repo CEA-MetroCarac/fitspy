1f861552f04702cda68836222023e3ca525168a2
patquem <patrick.quemere@cea.fr>
2026-03-25 16:54:11 +0100

    revisit 'interactive' baseline to avoid double plotting

--- fitspy/apps/pyside/components/plot/model.py
+++ fitspy/apps/pyside/components/plot/model.py
@@ -376,10 +376,7 @@ class Model(QObject):
         spectrum = self.current_spectra[0]
         baseline = spectrum.baseline
         if not baseline.is_subtracted:
-            x, y = spectrum.x, None
-            if baseline.attached or baseline.mode == "arpls":
-                y = spectrum.y
-            baseline.plot(ax, x, y, attached=baseline.attached)
+            baseline.plot(ax, spectrum.x, spectrum.y, attached=baseline.attached, label="_Baseline")
 
         self.lines = []
         first_spectrum = True
