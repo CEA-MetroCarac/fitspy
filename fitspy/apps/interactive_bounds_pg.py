9e12c4d9b94e49c79144dee50a65796614e0e138
patquem <patrick.quemere@cea.fr>
2026-03-26 15:16:50 +0100

    add update_params() and update_profiles()

--- fitspy/apps/interactive_bounds_pg.py
+++ fitspy/apps/interactive_bounds_pg.py
@@ -2,20 +2,22 @@
 module description
 """
 
-import numpy as np
 import pyqtgraph as pg
-from pyqtgraph.Qt import QtWidgets, QtCore, QtGui
+from pyqtgraph.Qt import QtWidgets, QtCore
 
 from fitspy.core.utils import closest_index
+from fitspy.apps.interactive_bounds import set_tmp_params, CMAP
+from fitspy.apps.pyside.utils import convert_color_pg
 
 
 class InteractiveBounds(QtCore.QObject):
 
-    def __init__(self, spectrum, plot_widget, model=None, bind_func=None):
+    def __init__(self, spectrum, plot_widget, cmap=None, model=None, bind_func=None):
         super().__init__()
 
         self.spectrum = spectrum
         self.plot_widget = plot_widget
+        self.cmap = cmap or CMAP
         self.model = model
         self.bind_func = bind_func
 
@@ -39,7 +41,7 @@ class InteractiveBounds(QtCore.QObject):
             for bbox in reversed(self.bboxes):
                 if bbox.contains(x, y):
                     self.active_bbox = bbox
-                    bbox.start_drag(x)
+                    bbox.start_drag(x, y)
                     self.plot_widget.getViewBox().setMouseEnabled(x=False, y=False)
                     return True
 
@@ -48,6 +50,16 @@ class InteractiveBounds(QtCore.QObject):
                     self.spectrum.add_peak_model(self.model, x)
                     bbox = BBox(self.plot_widget, self.spectrum, self.spectrum.peak_models[-1])
                     self.bboxes.append(bbox)
+
+        elif event.type() == QtCore.QEvent.GraphicsSceneMouseRelease:
+
+            if hasattr(self, "active_bbox") and self.active_bbox:
+                self.active_bbox.stop_drag()
+                self.active_bbox = None
+                self.plot_widget.getViewBox().setMouseEnabled(x=True, y=True)
+                if self.bind_func is not None:
+                    self.bind_func()
+                return True
 
         return False
 
@@ -69,18 +81,21 @@ class InteractiveBounds(QtCore.QObject):
         for bbox in self.bboxes:
             bbox.remove()
         self.bboxes = []
-        for peak_model in self.spectrum.peak_models:
+        for k, peak_model in enumerate(self.spectrum.peak_models):
             bbox = BBox(self.plot_widget, self.spectrum, peak_model)
+            bbox.set_color(self.cmap(k % self.cmap.N))
             self.bboxes.append(bbox)
 
 
 class BBox:
 
-    def __init__(self, plot_widget, spectrum, peak_model, color=(0, 0, 255, 80)):
+    def __init__(self, plot_widget, spectrum, peak_model, color='b', ratio=0.5):
 
         self.plot_widget = plot_widget
         self.spectrum = spectrum
         self.peak_model = peak_model
+        self.color = color
+        self.ratio = ratio
 
         self.view = plot_widget.getViewBox()
 
@@ -94,28 +109,31 @@ class BBox:
         self.dx0 = [self.x0 - params['x0']['min'], params['x0']['max'] - self.x0]
 
         if 'fwhm_l' in params:
+            self.fwhm = [params['fwhm_l']['value'], params['fwhm_r']['value']]
             self.dfwhm = [params['fwhm_l']['max'], params['fwhm_r']['max']]
             self.symetric = False
         else:
+            self.fwhm = [params['fwhm']['value'], params['fwhm']['value']]
             self.dfwhm = [params['fwhm']['max']] * 2
             self.symetric = True
 
-        self.color = color
-
-        pen = pg.mkPen(color[:3])
-        brush = pg.mkBrush(color)
+        pen = pg.mkPen(self.color)
+        brush = pg.mkBrush(self.color)
 
         self.line = pg.PlotDataItem([self.x0, self.x0], [0, self.ampli], pen=pen)
 
         # rectangles
         self.rect_x0 = QtWidgets.QGraphicsRectItem()
         self.rect_x0.setBrush(brush)
+        self.rect_x0.setOpacity(0.3)
 
         self.rect_x0_inner = QtWidgets.QGraphicsRectItem()
         self.rect_x0_inner.setBrush(brush)
+        self.rect_x0_inner.setOpacity(0.3)
 
         self.rect_fwhm = QtWidgets.QGraphicsRectItem()
         self.rect_fwhm.setBrush(brush)
+        self.rect_fwhm.setOpacity(0.3)
 
         self.plot_widget.addItem(self.line)
         self.view.addItem(self.rect_x0)
@@ -124,24 +142,34 @@ class BBox:
 
         self.update_display()
 
-    def start_drag(self, x):
+    def set_color(self, color):
+        self.color = convert_color_pg(color)
+        pen = pg.mkPen(self.color)
+        brush = pg.mkBrush(self.color)
+        self.line.setPen(pen)
+        for rect in [self.rect_x0, self.rect_x0_inner, self.rect_fwhm]:
+            rect.setPen(pen)
+            rect.setBrush(brush)
+        if self.tmp is not None:
+            self.tmp.setPen(pen)
+
+    def start_drag(self, x, y):
         self.last_x = x
         self.dragging = None
 
-        if self.contains_rect(self.rect_x0_inner, x, 0):
+        if self.contains_rect(self.rect_x0_inner, x, y):
             self.dragging = 'all'
 
-        elif self.contains_rect(self.rect_x0, x, 0):
+        elif self.contains_rect(self.rect_x0, x, y):
             self.dragging = 'dx0[0]' if x < self.x0 else 'dx0[1]'
 
-        elif self.contains_rect(self.rect_fwhm, x, 0):
+        elif self.contains_rect(self.rect_fwhm, x, y):
             self.dragging = 'dfwhm[0]' if x < self.x0 else 'dfwhm[1]'
 
     def stop_drag(self):
         self.dragging = None
 
     def on_move(self, x):
-
         if not self.dragging:
             return
 
@@ -182,22 +210,54 @@ class BBox:
         local_pos = rect.mapFromScene(scene_pos)
         return rect.rect().contains(local_pos)
 
-    def update_display(self):
-        ratio = 0.5
+    def update_params(self):
+        self.peak_model.set_param_hint('ampli', value=self.ampli)
+        self.peak_model.set_param_hint('x0', value=self.x0)
+        self.peak_model.set_param_hint('x0', min=self.x0 - self.dx0[0])
+        self.peak_model.set_param_hint('x0', max=self.x0 + self.dx0[1])
+        if 'fwhm_l' in self.peak_model.param_hints.keys():
+            self.peak_model.set_param_hint('fwhm_l', value=self.fwhm[0])
+            self.peak_model.set_param_hint('fwhm_r', value=self.fwhm[1])
+            self.peak_model.set_param_hint('fwhm_l', max=self.dfwhm[0])
+            self.peak_model.set_param_hint('fwhm_r', max=self.dfwhm[1])
+        else:
+            self.peak_model.set_param_hint('fwhm', value=self.fwhm[0])
+            self.peak_model.set_param_hint('fwhm', max=self.dfwhm[0])
 
+    def update_display(self):
         self.line.setData([self.x0, self.x0], [0, self.ampli])
 
-        x = self.x0 - self.dx0[0]
         w = self.dx0[0] + self.dx0[1]
-        h = ratio * self.ampli
-
-        self.rect_x0.setRect(x, 0, w, h)
+        h = self.ratio * self.ampli
+        self.rect_x0.setRect(self.x0 - self.dx0[0], 0, w, h)
         self.rect_x0_inner.setRect(self.x0 - 0.5 * self.dx0[0], 0, 0.5 * w, h)
-        self.rect_fwhm.setRect(self.x0 - self.dfwhm[0], ratio * self.ampli,
-                               0.5 * (self.dfwhm[0] + self.dfwhm[1]), (1 - ratio) * self.ampli)
+        self.rect_fwhm.setRect(self.x0 - 0.5 * self.dfwhm[0], self.ratio * self.ampli,
+                               0.5 * (self.dfwhm[0] + self.dfwhm[1]), (1 - self.ratio) * self.ampli)
+
+    def update_profiles(self):
+        if self.tmp is not None:
+            [x.remove() for x in self.tmp]
+        self.tmp = []
+
+        x = self.spectrum.x
+
+        with set_tmp_params(self.peak_model):
+            tmp_params = self.peak_model.make_params()
+            y = self.peak_model.eval(tmp_params, x=x)
+            self.tmp.append(self.ax.plot(x, y, c=self.color, lw=0.5)[0])
+
+        mask = (self.x0 - 0.5 * self.dfwhm[0] <= x) & (x <= self.x0 + 0.5 * self.dfwhm[1])
+        with set_tmp_params(self.peak_model, self.dfwhm):
+            tmp_params = self.peak_model.make_params()
+            y = self.peak_model.eval(tmp_params, x=x[mask])
+            self.tmp.append(self.ax.plot(x[mask], y, c=self.color, lw=0.5)[0])
 
 
 if __name__ == "__main__":
+    from pathlib import Path
+    from fitspy.core.spectrum import Spectrum
+    from fitspy.apps.pyside.components.plot.backend_manager import MplLikeAxes
+
     app = QtWidgets.QApplication([])
 
     win = pg.GraphicsLayoutWidget(show=True)
@@ -205,9 +265,7 @@ if __name__ == "__main__":
 
     plot = win.addPlot()
     plot.getViewBox().setMenuEnabled(False)
-
-    from fitspy.core.spectrum import Spectrum
-    from pathlib import Path
+    ax = MplLikeAxes(plot)
 
     DATA = Path(__file__).parents[2] / "examples" / "data"
     fname = DATA / 'spectra_2' / 'spectrum_2_1.txt'
@@ -215,7 +273,10 @@ if __name__ == "__main__":
     spectrum = Spectrum()
     spectrum.load_profile(fname=fname)
 
-    plot.plot(spectrum.x, spectrum.y)
+    ax.plot(spectrum.x, spectrum.y)
+    inds = spectrum.inds_local_minima()
+    for ind in inds:
+        ax.axvline(spectrum.x[ind], 'b', lw=0.3)
 
     ib = InteractiveBounds(spectrum, plot, model='Gaussian')
 
